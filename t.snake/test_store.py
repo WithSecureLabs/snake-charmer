1f2ebf3176a4ed459aaef462df46181724f10349
Alex Kornitzer <alex.kornitzer@countercept.com>
2018-03-09 13:04:03 +0000

    store: update for store change

--- t.snake/test_store.py
+++ t.snake/test_store.py
@@ -3,39 +3,39 @@ import requests
 
 def test_store_get(regtest, db, url, file, memory):
     r = requests.get(url + '/store')
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_file(regtest, db, url, file, memory):
     data = "?file_type=file"
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_memory(regtest, db, url, file, memory):
     data = "?file_type=memory"
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_filter(regtest, db, url, file, memory):
     data = "?filter[name]=" + file['name']
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_filter_and(regtest, db, url, file, memory):
     data = "?filter[name]=" + file['name'] + \
            "&filter[name]=" + memory['name']
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_filter_and_1(regtest, db, url, file, memory):
     data = "?filter[name]=" + file['name'] + \
            "&filter[file_type]=" + file['file_type']
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_filter_or(regtest, db, url, file, memory):
@@ -43,10 +43,10 @@ def test_store_filter_or(regtest, db, url, file, memory):
            "&filter[name]=" + memory['name'] + \
            "&operator=or"
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
 
 
 def test_store_limit(regtest, db, url, file, memory):
     data = "?limit=1"
     r = requests.get(url + '/store' + data)
-    regtest.write(str(len(r.json()['data']['store'])))
+    regtest.write(str(len(r.json()['data']['samples'])))
